fac91c6c73e8281438e7507646fa435350973ad4
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-01-02 18:30:32 +0200

    test

--- PID1/PID1.py
+++ PID1/PID1.py
@@ -1,9 +1,8 @@
-# from github import Github
+from github import Github
 
 def main():
     print('GitHub API') 
-    # g = Github("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")
-    # organization = g.get_organization("DamianoGlobal")
-    # organization.create_repo("Some new repository.")
-
-    # print(g)
+    g = Github("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")
+    organization = g.get_organization("DamianoGlobal")
+    organization.create_repo("Some new repository.")
+    print(g)
